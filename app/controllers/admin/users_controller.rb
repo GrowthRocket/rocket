418395bdde5f39c74f21e0dcae493a38570fde74
liyanzeng <yanzengli@gmail.com>
2016-08-16 19:07:58 +0800

    admin can edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,5 +4,35 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def new
+    @user = User.new
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email)
+  end
 end
