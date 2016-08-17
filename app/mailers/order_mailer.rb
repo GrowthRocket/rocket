35990f3d817984d89d2ad439a39e90250ee72cb7
Rebecca Wang <wangmmqq@gmail.com>
2016-08-17 14:56:53 +0800

    add notify_order_placed_email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,8 @@ class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order = order
     @user = order.user
-
-    mail(to: @user.email, subject: "[Rocket] 感谢你支持@project.name"
+    @project = @order.project
+    @title = @project.name
+    mail(to: @user.email, subject: "[Rocket] 感谢你支持 #{@title}项目")
   end
 end
