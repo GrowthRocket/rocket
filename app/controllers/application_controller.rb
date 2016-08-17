568fb1f0a6607b7602e2d8c9f5cd8697ef79347d
nfreeness <nfreeness@gmail.com>
2016-08-17 14:21:16 +0800

    fix user log_in/sign_up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to root_path
     end
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute,:user_name])
   end
 end
