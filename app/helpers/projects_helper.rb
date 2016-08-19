eb3116656a7c171c46f0cc3f740baefb077fce80
xdite <xuite.joke@gmail.com>
2016-08-19 10:21:58 +0800

    render_project_image

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,9 @@
 module ProjectsHelper
   def render_project_description(project)
-     simple_format(project.description)
+    simple_format(project.description)
+  end
+
+  def render_project_image(project, _size = :thumb)
+    image_tag(project.image.thumb, width: 123, height: 90)
   end
 end
