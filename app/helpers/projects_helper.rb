f554f2a1302e31aba6b3d0fc92d1775e1374f5bc
xdite <xuite.joke@gmail.com>
2016-08-19 10:23:25 +0800

    render project funding progress

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,4 +6,8 @@ module ProjectsHelper
   def render_project_image(project, _size = :thumb)
     image_tag(project.image.thumb, width: 123, height: 90)
   end
+
+  def render_project_funding_progress(project)
+    number_with_precision(project.fund_progress.to_f / project.fund_goal * 100, precision: 2) + ' %'
+  end
 end
