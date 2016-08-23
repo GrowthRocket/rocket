9dfa00fba4eba14f2e2cfb2bea6170c3e71fab6a
xdite <xuite.joke@gmail.com>
2016-08-23 20:17:47 +0800

    reformat code

--- lib/geetest_ruby_sdk.rb
+++ lib/geetest_ruby_sdk.rb
@@ -1,31 +1,35 @@
-require 'digest'
-require 'net/http'
+require "digest"
+require "net/http"
 
 class GeetestSDK
-	def initialize(key, new_uri=nil)
-		@gee_uri = new_uri || 'http://api.geetest.com/validate.php'
-		@key = key
-	end
+  def initialize(key, new_uri = nil)
+    @gee_uri = new_uri || "http://api.geetest.com/validate.php"
+    @key = key
+  end
 
-	def validate(challenge='', validate='', seccode='')
-		md5 = Digest::MD5.hexdigest(@key + 'geetest' + challenge)
-		if validate == md5
-			back = post(@gee_uri, seccode: seccode) rescue ''
-			if back == Digest::MD5.hexdigest(seccode)
-				return true
-			else
-				return false
-			end #if
-		else
-			return false
-		end #if
-	end #geetest_validate
+  def validate(challenge = "", validate = "", seccode = "")
+    md5 = Digest::MD5.hexdigest(@key + "geetest" + challenge)
+    if validate == md5
+      back =
+        begin
+          post(@gee_uri, seccode: seccode)
+        rescue
+          ""
+        end
+      if back == Digest::MD5.hexdigest(seccode)
+        return true
+      else
+        return false
+      end # if
+    else
+      return false
+    end # if
+  end # geetest_validate
 
-	# data.is_a?(Hash)==true
-	def post(uri, data)
-		uri = URI(uri)
-		res = Net::HTTP.post_form(uri, data)
-		res.body
-	end
-
+  # data.is_a?(Hash)==true
+  def post(uri, data)
+    uri = URI(uri)
+    res = Net::HTTP.post_form(uri, data)
+    res.body
+  end
 end
