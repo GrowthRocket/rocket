5d8d56c2ae2249525966ba597bc96ee25d5e1b6c
liyanzeng <yanzengli@gmail.com>
2016-08-23 17:17:02 +0800

    modify-user-identity-verify

--- app/controllers/admin/users_verify_controller.rb
+++ app/controllers/admin/users_verify_controller.rb
@@ -14,7 +14,7 @@ class Admin::UsersVerifyController < ApplicationController
   def pass_verify
     @user = User.find(params[:id])
     @user.approve!
-    @user.aasm_state = "passed_verify"
+    @user.aasm_state = "passed_verified"
     @user.save
     flash[:notice] = "已通过该用户的实名认证申请!"
     redirect_to :back
@@ -24,7 +24,7 @@ class Admin::UsersVerifyController < ApplicationController
   def reject_verify
     @user = User.find(params[:id])
     @user.reject!
-    @user.aasm_state = "unverified"
+    @user.aasm_state = "unpassed_verified"
     @user.save
     flash[:notice] = "已拒绝该用户的实名认证申请!"
     redirect_to :back
