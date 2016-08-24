11495ea602448c4c65d5771306a2f6b417498cd0
nfreeness <nfreeness@gmail.com>
2016-08-24 13:16:26 +0800

    fix project dispay detail

--- app/helpers/account/projects_helper.rb
+++ app/helpers/account/projects_helper.rb
@@ -1,14 +1,15 @@
 module Account::ProjectsHelper
   def render_project_operation(project)
     if project.aasm_state == "project_created" && @projects.where("aasm_state = ? OR aasm_state = ?", "online", "verifying").count == 0
-      link_to("上线", apply_for_verification_account_project_path(project), :method => :post,  :class => "btn btn-xs btn-info")
+      link_to("申请发布", apply_for_verification_account_project_path(project), :method => :post,  :class => "btn btn-xs btn-info")
     elsif project.aasm_state == "online"
       link_to("下线", offline_account_project_path(project), :method => :post,  :class => "btn btn-xs btn-info")
     elsif project.aasm_state == "unverified"
       # link_to("查看审核详情", check_info_account_project_path(project), :method => :post,  :class => "btn btn-xs btn-info")
-      link_to("查看审核详情", "#", :method => :post,  :class => "btn btn-xs btn-info disabled")
+      link_to("申请发布", apply_for_verification_account_project_path(project), :method => :post,  :class => "btn btn-xs btn-info")
     else
       ""
     end
   end
+
 end
