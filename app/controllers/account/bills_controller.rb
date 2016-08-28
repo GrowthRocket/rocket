ab31d7c04ead94edf4a5577b910ded3b773ac5db
shaojunda <shaojunda@gmail.com>
2016-08-28 12:57:34 +0800

    just show profit for user

--- app/controllers/account/bills_controller.rb
+++ app/controllers/account/bills_controller.rb
@@ -3,6 +3,7 @@ class Account::BillsController < AccountController
     @user = current_user
     @projects = @user.projects
     @payments = BillPayment.where(bill_status: %w(success paid), project_id: @projects)
-    @payments_amount = @payments.sum(:amount)
+    # @payments_amount = @payments.sum(:amount)
+    @account = Account.find_by_user_id(current_user.id)
   end
 end
