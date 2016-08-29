2be54b8617c9c6faea542a02fbae02e0400c69bc
Rebecca Wang <wangmmqq@gmail.com>
2016-08-29 18:54:22 +0800

    edit project part

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+  def render_progress_landing_page(percent)
+    percent = percent.to_s + "%"
+    content_tag :div, class: "progress progress-whole" do
+      content_tag :div, class: %w(progress-bar progress-content), role: "progressbar", 'aria-valuenow': percent, 'aria-valuemin': "0", 'aria-valuemax': "100", 'style': "width: "+ percent + ";min-width:2em;"  do
+      end
+    end
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -56,6 +56,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :large do
     process resize_to_fill: [750, 410]
   end
+
+  version :landing_page do
+    process resize_to_fill: [320, 200]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
