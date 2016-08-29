df66f3958b2cf152ac10ff63ed9464f7341365c4
xdite <xuite.joke@gmail.com>
2016-08-30 02:29:35 +0800

    拆開成 payout service

--- app/controllers/admin/bills_controller.rb
+++ app/controllers/admin/bills_controller.rb
@@ -65,8 +65,7 @@ class Admin::BillsController < AdminController
     @project = Project.find(params[:id])
     amount = BillPayment.where(bill_status: "success", project_id: params[:id]).sum(:amount)
     final_amount = ((100 - ENV["fund_rate"].to_f) / 100) * amount
-    options = { project: @project, amount: final_amount }
-    if FundingService.new(options).payout!
+    if PayoutService.new(@project, final_amount).perform!
       BillPayment.where(project_id: params[:id]).update_all(bill_status: "paid")
       flash[:notice] = "资金发放成功！"
     else
--- app/services/funding_service.rb
+++ app/services/funding_service.rb
@@ -1,14 +1,10 @@
 class FundingService
-  # def initialize(order, user, project)
-  def initialize(options)
-    # FIXME : too ugly
-    @order = options[:order] unless options[:order].nil?
-    @project = @order.project unless options[:order].nil?
-    @plan = @order.plan unless options[:order].nil?
-    @user = options[:user] unless options[:user].nil?
-    @project = options[:project] unless options[:project].nil?
-    @amount = options[:amount] unless options[:amount].nil?
-    @payment_method = options[:payment_method] unless options[:payment_method].nil?
+  def initialize(order, user, payment_method)
+    @order = order
+    @project = @order.project
+    @plan = @order.plan
+    @user = user
+    @payment_method = payment_method
   end
 
   # TODO: 需要加锁、加事务
@@ -38,18 +34,6 @@ class FundingService
     @account = @user.account
     @account.amount += @order.total_price
     @account.save
-  end
-
-  def payout!
-    user = @project.user
-    account = user.account
-    account.profit += @amount
-    account.save
-
-    BillPayout.create(
-      project_id: @project.id, amount: @amount, account_name: account.account_name,
-      user_id: user.id, bill_status: "paid", project_name: @project.name, creator_name: user.user_name
-    )
   end
 
   def add_progress!
