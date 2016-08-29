0e7f6c64ca0f6d8e8e86400b4ea338f16c7b43c7
xdite <xuite.joke@gmail.com>
2016-08-30 02:33:08 +0800

    add scope

--- app/services/funding_service.rb
+++ app/services/funding_service.rb
@@ -11,12 +11,12 @@ class FundingService
   def add!
     @project.fund_progress += @order.total_price
     # @user.orders.where("orders_count = ? AND locked = ?", params[:orders], false)
-    if @user.orders.where(project_id: @order.project, aasm_state: "paid").empty?
+    if @user.orders.where(project_id: @order.project).paid.empty?
       @project.backer_quantity += 1
     end
     @project.save
 
-    if @user.orders.where(plan_id: @order.plan, aasm_state: "paid").empty?
+    if @user.orders.where(plan_id: @order.plan).paid.empty?
       @plan.backer_quantity += 1
     else
       @user.orders.where(plan_id: @order.plan)
