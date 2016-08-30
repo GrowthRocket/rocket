4ddf943ae67ee62b646d39701f9d6fac2377f334
Rebecca Wang <wangmmqq@gmail.com>
2016-08-30 13:15:15 +0800

    edit plan order

--- app/helpers/account/posts_helper.rb
+++ app/helpers/account/posts_helper.rb
@@ -1,5 +1,5 @@
 module Account::PostsHelper
   def render_post_description(post)
-    truncate(simple_format(post.description), escape: false, length: 20)
+    truncate(sanitize(post.description), escape: false, length: 20)
   end
 end
