d907080c4b2009875276487767f55ed8d3635d9b
Rebecca Wang <wangmmqq@gmail.com>
2016-09-01 21:50:15 +0800

    add project#show page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -21,4 +21,13 @@ module ProjectsHelper
       content_tag :span, "暂时还没有以这个名称命名的项目哦，请换一个项目名字再试一下"
     end
   end
+
+  def render_progress_show(percent)
+    percent = percent.to_s + "%"
+    content_tag :div, class: "progress sh-progress-whole" do
+      content_tag :div, class: %w(progress-bar sh-progress-content), role: "progressbar", 'aria-valuenow': percent, 'aria-valuemin': "0", 'aria-valuemax': "100", 'style': "width: "+ percent + ";min-width:2em;"  do
+      end
+    end
+  end
+
 end
