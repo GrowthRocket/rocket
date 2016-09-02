f7559d75cc0024156017e02a7e50cc7a945ff986
shaojunda <shaojunda@gmail.com>
2016-09-02 10:24:59 +0800

    fix error message don't show

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,6 +1,6 @@
 class Account::UsersController < ApplicationController
   before_action :authenticate_user!, except: [:send_verification_code]
-  before_action :check_geetest, only: [:send_verification_code]
+  # before_action :check_geetest, only: [:send_verification_code]
   before_action :phone_number_validates, only: [:verify_phone_number]
   before_action :phone_number_validates_new, only: [:verify_phone_number_new]
   layout "user"
