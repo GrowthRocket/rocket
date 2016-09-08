091138187f51df8664ab1bf55d3cfc5c321fce1a
shaojunda <shaojunda@gmail.com>
2016-09-08 22:24:18 +0800

    add browser check

--- app/assets/javascripts/account/projectFlow.js
+++ app/assets/javascripts/account/projectFlow.js
@@ -13,25 +13,30 @@ function showError(error, containerId) {
 }
 
 $(function() {
+  bo = isBrowser();
   flowType = $("#flowType").val();
   projectId = $("#projectId").val();
   var multiple = 1;
   var element = document.querySelector("trix-editor");
-  $('trix-editor').on('trix-change', function() {
-    if (projectId == undefined) {
-      var inputv =  $("#project_description_trix_input_project");
-      if (inputv.val().length == 60 * multiple) {
-        multiple++;
-        element.editor.insertLineBreak();
+
+  if (bo != undefined && bo == 'firefox') {
+    $('trix-editor').on('trix-change', function() {
+      if (projectId == undefined) {
+        var inputv =  $("#project_description_trix_input_project");
+        if (inputv.val().length == 50 * multiple) {
+          multiple++;
+          element.editor.insertLineBreak();
+        }
+      } else {
+        var inputv =  $("#project_description_trix_input_project_" + projectId);
+        if (inputv.val().length == 50 * multiple) {
+          multiple++;
+          element.editor.insertLineBreak();
+        }
       }
-    } else {
-      var inputv =  $("#project_description_trix_input_project_" + projectId);
-      if (inputv.val().length == 60 * multiple) {
-        multiple++;
-        element.editor.insertLineBreak();
-      }
-    }
-  })
+    })
+
+  }
 
   if (flowType == "edit") {
     $("input[name='_method']").val("post");
@@ -443,4 +448,63 @@ function generatePlanB(planPrice, planGoal, planDescription, planIndex, planNum,
               "</div>" +
             "</div>";
   return content;
+}
+
+
+function isBrowser(){
+
+    var Sys={};
+
+    var ua=navigator.userAgent.toLowerCase();
+
+    var s;
+
+    (s=ua.match(/msie ([\d.]+)/))?Sys.ie=s[1]:
+
+    (s=ua.match(/firefox\/([\d.]+)/))?Sys.firefox=s[1]:
+
+    (s=ua.match(/chrome\/([\d.]+)/))?Sys.chrome=s[1]:
+
+    (s=ua.match(/opera.([\d.]+)/))?Sys.opera=s[1]:
+
+    (s=ua.match(/version\/([\d.]+).*safari/))?Sys.safari=s[1]:0;
+
+    // if(Sys.ie){//Js判断为IE浏览器
+    //
+    //     alert('http://www.phpernote.com'+Sys.ie);
+    //
+    //     if(Sys.ie=='9.0'){//Js判断为IE 9
+    //
+    //     }else if(Sys.ie=='8.0'){//Js判断为IE 8
+    //
+    //     }else{
+    //
+    //     }
+    //
+    // }
+
+    if(Sys.firefox){//Js判断为火狐(firefox)浏览器
+
+        return "firefox"
+
+    }
+    //
+    // if(Sys.chrome){//Js判断为谷歌chrome浏览器
+    //
+    //     alert('http://www.phpernote.com'+Sys.chrome);
+    //
+    // }
+    //
+    // if(Sys.opera){//Js判断为opera浏览器
+    //
+    //     alert('http://www.phpernote.com'+Sys.opera);
+    //
+    // }
+    //
+    // if(Sys.safari){//Js判断为苹果safari浏览器
+    //
+    //     alert('http://www.phpernote.com'+Sys.safari);
+    //
+    // }
+
 }
